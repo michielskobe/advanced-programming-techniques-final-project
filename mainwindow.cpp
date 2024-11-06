0ee3d69e74dfa4a5926d1fc62181b471a25daf8d
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2024-11-06 14:28:07 +0100

    Add picture to window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGraphicsScene>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QGraphicsScene *scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);;
+    scene->addPixmap(QPixmap(":/images/test.jpg"));
 }
 
 MainWindow::~MainWindow()
