33664466d11c33fac8f628d223ba64f01da675fa
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2024-12-18 14:00:29 +0100

    Added simple enemy animation

--- graphicalenemyview.cpp
+++ graphicalenemyview.cpp
@@ -9,16 +9,27 @@ GraphicalEnemyView::GraphicalEnemyView(QGraphicsScene* scene)
 
 void GraphicalEnemyView::updateView() {
     for (auto& enemy : (*levels)[*(gameController->getActiveLevelIndex())]->enemies) {
-        QString image = ":/images/test2.jpg";
-        auto temp = dynamic_cast<PEnemy*>(&(*enemy));
-        if (temp != nullptr){
-            image = ":/images/enemy.png";
+        QString image = ":/images/Enemy_Idle.png";
+        if (enemy->getDefeated()){
+            image = ":/images/Enemy_Dead.png";
+            auto temp = dynamic_cast<PEnemy*>(&(*enemy));
+            if (temp != nullptr){
+                image = ":/images/PEnemy_Dead.png";
+            }
+            auto temp2 = dynamic_cast<XEnemy*>(&(*enemy));
+            if (temp2 != nullptr){
+                image = ":/images/XEnemy.png";
+            }
+        } else {
+            auto temp = dynamic_cast<PEnemy*>(&(*enemy));
+            if (temp != nullptr){
+                image = ":/images/PEnemy_Idle.png";
+            }
+            auto temp2 = dynamic_cast<XEnemy*>(&(*enemy));
+            if (temp2 != nullptr){
+                image = ":/images/XEnemy.png";
+            }
         }
-        auto temp2 = dynamic_cast<XEnemy*>(&(*enemy));
-        if (temp2 != nullptr){
-            image = ":/images/XEnemy.png";
-        }
-
         QGraphicsPixmapItem* enemyPixmapItem = new QGraphicsPixmapItem(QPixmap(image));
         enemyPixmapItem->setPos(enemy->getXPos() * 50, enemy->getYPos() * 50);
         enemyPixmapItem->setZValue(1);
