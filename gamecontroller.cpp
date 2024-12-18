23c32d01728a3edd6be3ec6189fe54597fd30cda
Robbe Decapmaker <debber@dcpm.be>
2024-12-18 15:52:39 +0100

    Using more realistic map for testing

--- gamecontroller.cpp
+++ gamecontroller.cpp
@@ -219,7 +219,7 @@ void GameController::initialGameLoad()
     // get levels from GameGenerator;
     std::vector<QString> fileNames;
     fileNames.push_back(":/images/world_images/worldmap.png");
-    fileNames.push_back(":/images/world_images/maze1.png");
+    fileNames.push_back(":/images/world_images/worldmap4.png");
 
     LevelManager* levelManager = LevelManager::GetInstance();
     levelManager->setLevels(fileNames);
