ae93c29ab140f88e0f99bf25ce87e938a190f42a
Robbe Decapmaker <debber@dcpm.be>
2024-12-27 18:49:33 +0100

    Fix memory leak on graphicalOverlayView

--- graphicaloverlayview.cpp
+++ graphicaloverlayview.cpp
@@ -6,10 +6,13 @@ GraphicalOverlayView::GraphicalOverlayView(QGraphicsScene* scene)
     : GraphicalView(scene) {
     levels = LevelManager::GetInstance()->getLevels();
     gameController = GameController::GetInstance();
+    QString imageFile = (*levels)[*(gameController->activeLevelIndex)]->worldOverlayLocation;
+    characterPixmapItem = new QGraphicsPixmapItem(QPixmap(imageFile));
 }
 
 void GraphicalOverlayView::updateView(){
     QString imageFile = (*levels)[*(gameController->activeLevelIndex)]->worldOverlayLocation;
+    delete characterPixmapItem; // prevent memory leak when loading new imageFile
     characterPixmapItem = new QGraphicsPixmapItem(QPixmap(imageFile));
     characterPixmapItem->setScale(positionScalingFactor);
     characterPixmapItem->setZValue(1);
